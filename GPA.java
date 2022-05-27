978b85b29d03097f057f7d52f3b9f2d3da137305
Brodie Friday <brodieleefriday@gmail.com>
2022-05-27 10:58:47 -0500

    Added grade point text fields and added if statements to actionperformed method
    Also added some JLabels and fixed references to generic type for JComboBoxes

--- GPA.java
+++ GPA.java
@@ -5,8 +5,9 @@ import java.awt.event.*;
 
 public class GPA extends JFrame implements ActionListener{
 
-   JComboBox class1Grade,class2Grade,class3Grade,class4Grade,class5Grade,class6Grade;
-   JTextField class1CreditHrs,class2CreditHrs,class3CreditHrs,class4CreditHrs,class5CreditHrs,class6CreditHrs;
+   JComboBox<String> class1Grade,class2Grade,class3Grade,class4Grade,class5Grade,class6Grade;
+   JTextField class1CreditHrs,class2CreditHrs,class3CreditHrs,class4CreditHrs,class5CreditHrs,class6CreditHrs,class1GradePoints
+   ,gradePoints,class2GradePoints,class3GradePoints,class4GradePoints,class5GradePoints,class6GradePoints;
 
     public GPA(){
 
@@ -21,8 +22,13 @@ public class GPA extends JFrame implements ActionListener{
       // Title left column
       JLabel creditHours = new JLabel("Credit Hours");
       GPAContainer.add(creditHours);
-      creditHours.setBounds(90, 75, 100, 20);
-      // Left column of textboxes for credit hours
+      creditHours.setBounds(85, 80, 100, 20);
+      // Title right column
+      JLabel gradePoints = new JLabel("Grade Points");
+      GPAContainer.add(gradePoints);
+      gradePoints.setBounds(335, 80, 100, 20);
+
+      // Left column of textboxes for credit hours (6 total)
        class1CreditHrs= new JTextField("");
        GPAContainer.add(class1CreditHrs);
        class1CreditHrs.setBounds(80, 100, 100, 20);
@@ -43,10 +49,34 @@ public class GPA extends JFrame implements ActionListener{
        GPAContainer.add(class5CreditHrs);
        class5CreditHrs.setBounds(80, 220, 100, 20);
        
-       class6CreditHrs= new JTextField("");
+       class6CreditHrs = new JTextField("");
        GPAContainer.add(class6CreditHrs);
        class6CreditHrs.setBounds(80, 250, 100, 20);
-      
+      // Right column of textboxes for grade points (6 total)
+       class1GradePoints = new JTextField("");
+       GPAContainer.add(class1GradePoints);
+       class1GradePoints.setBounds(330, 100, 100, 20);
+
+       class2GradePoints = new JTextField("");
+       GPAContainer.add(class2GradePoints);
+       class2GradePoints.setBounds(330, 130, 100, 20);
+
+       class3GradePoints = new JTextField("");
+       GPAContainer.add(class3GradePoints);
+       class3GradePoints.setBounds(330, 160, 100, 20);
+
+       class4GradePoints = new JTextField("");
+       GPAContainer.add(class4GradePoints);
+       class4GradePoints.setBounds(330, 190, 100, 20);
+
+       class5GradePoints = new JTextField("");
+       GPAContainer.add(class5GradePoints);
+       class5GradePoints.setBounds(330, 220, 100, 20);
+
+       class6GradePoints = new JTextField("");
+       GPAContainer.add(class6GradePoints);
+       class6GradePoints.setBounds(330, 250, 100, 20);
+
       // Button for calculating the grade points
       JButton calc = new JButton("Calculate");
       GPAContainer.add(calc);
@@ -63,46 +93,72 @@ public class GPA extends JFrame implements ActionListener{
      
       
       // Comboboxes for picking grade
-      class1Grade = new JComboBox(tempLabels);
+      class1Grade = new JComboBox<>(tempLabels);
       GPAContainer.add(class1Grade);
-      class1Grade.setBounds(185,95,120,40);
+      class1Grade.setBounds(185,92,120,40);
       class1Grade.setVisible(true);
       
-      class2Grade = new JComboBox(tempLabels);
+      class2Grade = new JComboBox<>(tempLabels);
       GPAContainer.add(class2Grade);
-      class2Grade.setBounds(185,125,120,40);
+      class2Grade.setBounds(185,122,120,40);
       class2Grade.setVisible(true);
       
-      class3Grade = new JComboBox(tempLabels);
+      class3Grade = new JComboBox<>(tempLabels);
       GPAContainer.add(class3Grade);
-      class3Grade.setBounds(185,155,120,40);
+      class3Grade.setBounds(185,152,120,40);
       class3Grade.setVisible(true);
       
-      class4Grade = new JComboBox(tempLabels);
+      class4Grade = new JComboBox<>(tempLabels);
       GPAContainer.add(class4Grade);
-      class4Grade.setBounds(185,185,120,40);
+      class4Grade.setBounds(185,182,120,40);
       class4Grade.setVisible(true);
       
-      class5Grade = new JComboBox(tempLabels);
+      class5Grade = new JComboBox<>(tempLabels);
       GPAContainer.add(class5Grade);
-      class5Grade.setBounds(185,215,120,40);
+      class5Grade.setBounds(185,212,120,40);
       class5Grade.setVisible(true);
+
+      class6Grade = new JComboBox<>(tempLabels);
+      GPAContainer.add(class6Grade);
+      class6Grade.setBounds(185,242,120,40);
+      class6Grade.setVisible(true);
       
    }
    public void actionPerformed(ActionEvent e) {
       Calculation calc = new Calculation();
       DecimalFormat decFormat = new DecimalFormat("0.0");
-      int class1Hrs = Integer.parseInt(class1CreditHrs.getText());
-      int class2Hrs = Integer.parseInt(class2CreditHrs.getText());
-      int class3Hrs = Integer.parseInt(class3CreditHrs.getText());
-      int class4Hrs = Integer.parseInt(class4CreditHrs.getText());
-      int class5Hrs = Integer.parseInt(class5CreditHrs.getText());
-      int class6Hrs = Integer.parseInt(class6CreditHrs.getText());
+      int class1Hrs = 0;
+      int class2Hrs = 0;
+      int class3Hrs = 0;
+      int class4Hrs = 0;
+      int class5Hrs = 0;
+      int class6Hrs = 0;
+      if(!class1CreditHrs.getText().equals("")){
+        class1Hrs = Integer.parseInt(class1CreditHrs.getText());
+      }
+      if(!class2CreditHrs.getText().equals("")){
+        class2Hrs = Integer.parseInt(class2CreditHrs.getText());
+      }
+      if(!class3CreditHrs.getText().equals("")){
+        class3Hrs = Integer.parseInt(class3CreditHrs.getText());
+      }
+      if(!class4CreditHrs.getText().equals("")){
+        class4Hrs = Integer.parseInt(class4CreditHrs.getText());
+      }
+      if(!class5CreditHrs.getText().equals("")){
+        class5Hrs = Integer.parseInt(class5CreditHrs.getText());
+      }
+      if(!class6CreditHrs.getText().equals("")){
+        class6Hrs = Integer.parseInt(class6CreditHrs.getText());
+      }
       String grade = (String)class1Grade.getSelectedItem();
 
            if (e.getActionCommand().equals("Calculate")) {
                double c = calc.gradePoints(Double.parseDouble(class1CreditHrs.getText()),calc.QualityPoints(grade));
-               class1CreditHrs.setText(""+decFormat.format(c));
+               class1GradePoints.setText(""+decFormat.format(c));
+
+
+
                int totalCredits = calc.totalCredits(class1Hrs,class2Hrs,class3Hrs,class4Hrs,class5Hrs,class6Hrs);
                System.out.println(totalCredits);
            }
@@ -112,7 +168,7 @@ public class GPA extends JFrame implements ActionListener{
    
         GPA GPAFrame = new GPA();
         GPAFrame.setTitle("GPA calculator");
-        GPAFrame.setSize(420,450);
+        GPAFrame.setSize(450,450);
         GPAFrame.setVisible(true);
         GPAFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
